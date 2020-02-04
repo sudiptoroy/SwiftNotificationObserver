5eda1be72b9c49d1e5c9f8d553cd17da7375416b
Mac <sudiptoroy@sudipto.local>
2020-02-04 15:44:57 +0600

    Style: Add view controllers

---
+++ SwiftNotificationObserver/TextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TextViewController.swift
+//  SwiftNotificationObserver
+//
+//  Created by Sudipto Roy on 2/4/20.
+//  Copyright © 2020 Code-X Systems. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SwiftNotificationObserver/ViewController.swift
+++ SwiftNotificationObserver/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        
+    }
+    
 }
 
